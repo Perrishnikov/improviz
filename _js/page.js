0dc71715cb88df82f05c1bcb7c87a0ca659762e0
Perrishnikov <perrishnikov@gmail.com>
2016-04-21 22:12:16 -0500

    update 1

--- _js/page.js
+++ _js/page.js
@@ -3,38 +3,87 @@
 
 /* ---------- READY ---------- */
 $(document).ready(function(){
-	$('.grid').masonry({
-	  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
-	  columnWidth: '.grid-sizer',
-	  percentPosition: true
-	});
+	$('.Collage').removeWhitespace().collagePlus();
+	$('.Collage').collagePlus( {
+            /*
+             * The ideal height you want your row to be. It won't set it exactly to this as
+             * plugin adjusts the row height to get the correct width
+             */
+            'targetHeight'    : 400,
 
-	$('.grid').on('click', function(e){
-		console.log(e);
-		console.dir(e);
-	});
+            /*
+             * how quickly you want images to fade in once ready can be in ms, "slow" or "fast"
+             * This is only used in the default fade in effect. Timing of the other effects is
+             * controlled in CSS
+             */
+            'fadeSpeed'       : "slow",
 
-	(function revealMenu() {	//reveal Menu button (starts hidden in html)
+            /*
+             * which effect you want to use for revealing the images (note CSS3 browsers only),
+             * Options are effect-1 to effect-6 but you can also code your own
+             * Default is the safest option for supporting older browsers
+             */
+            'effect'          : 'effect-6',
 
-		$(window).keydown(function(event) {
-			if ( event.which == 77 ) {
-				$("#menu").toggle()
-				event.preventDefault();
-			}
-		});
-	}());
+            /*
+             * vertical: effects applied per row to give the impression of descending appearance
+             * horizontal: effects applied in order of appearance in the row to give a horizontal appearance
+             */
+            'direction'       : 'horizontal',
+
+            /*
+            * Sometimes there is just one image on the last row and it gets blown up to a huge size to fit the
+            * parent div width. To stop this behaviour, set this to true
+            */
+            'allowPartialLastRow'       : true
+        }
+    );
+
 
 });
 
 
 /* ---------- RESIZE ---------- */
 $(window).resize(function(){
+	$('.Collage').collagePlus( {
+			/*
+			 * The ideal height you want your row to be. It won't set it exactly to this as
+			 * plugin adjusts the row height to get the correct width
+			 */
+			'targetHeight'    : 400,
 
+			/*
+			 * how quickly you want images to fade in once ready can be in ms, "slow" or "fast"
+			 * This is only used in the default fade in effect. Timing of the other effects is
+			 * controlled in CSS
+			 */
+			'fadeSpeed'       : "slow",
+
+			/*
+			 * which effect you want to use for revealing the images (note CSS3 browsers only),
+			 * Options are effect-1 to effect-6 but you can also code your own
+			 * Default is the safest option for supporting older browsers
+			 */
+			'effect'          : 'effect-6',
+
+			/*
+			 * vertical: effects applied per row to give the impression of descending appearance
+			 * horizontal: effects applied in order of appearance in the row to give a horizontal appearance
+			 */
+			'direction'       : 'horizontal',
+
+			/*
+			* Sometimes there is just one image on the last row and it gets blown up to a huge size to fit the
+			* parent div width. To stop this behaviour, set this to true
+			*/
+			'allowPartialLastRow'       : true
+		}
+	);
 });
 
 /* ------------------------- FUNCTIONS ---------------------------- */
 
-function revealMenu() {	//reveal Menu button (starts hidden in html)
+(function revealMenu() {	//reveal Menu button (starts hidden in html)
 
 	$(window).keydown(function(event) {
 		if ( event.which == 77 ) {
@@ -42,4 +91,4 @@ function revealMenu() {	//reveal Menu button (starts hidden in html)
 			event.preventDefault();
 		}
 	});
-}
+}());
